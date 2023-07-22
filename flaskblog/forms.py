05205146b56820907f457dbc2695b94fed60717f
srishti <srishtiv424@gmail.com>
2023-07-22 10:00:45 +0530

    Initial commit

---
+++ flaskblog/forms.py
@@ -0,0 +1,53 @@
+from flask_wtf import FlaskForm
+from .models import User, Post
+from flask_wtf.file import FileField , FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from flask_login import current_user
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(min = 2 , max = 20)])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self , username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError("Username already taken")
+
+    def validate_email(self , email):
+        email = User.query.filter_by(email = email.data).first()
+        if email:
+            raise ValidationError("This email is already registered.")
+        
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(min = 2 , max = 20)])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self , username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError("Username already taken")
+
+    def validate_email(self , email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email = email.data).first()
+            if email:
+                raise ValidationError("This email is already registered.")
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
+    remember = BooleanField('Rember me')
+    submit = SubmitField('Login')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
